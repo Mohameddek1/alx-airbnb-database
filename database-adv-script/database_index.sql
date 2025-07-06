9d10d31de3f4204262b629cc4bb5151528f865d2
Mohameddek1 <mohadek2004@gmail.com>
2025-07-06 19:29:49 +0300

    alx-adv-script

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,14 +1,25 @@
--- Indexes for Optimization in Airbnb Schema
-
--- Users Table
-CREATE INDEX idx_users_email ON users(email);
+-- Index for user ID
 CREATE INDEX idx_users_id ON users(id);
 
--- Bookings Table
+-- Index for booking's user ID
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+
+-- Index for booking's property ID
 CREATE INDEX idx_bookings_property_id ON bookings(property_id);
+
+-- Index for booking's start date
 CREATE INDEX idx_bookings_start_date ON bookings(start_date);
 
--- Properties Table
+-- Index for property ID
 CREATE INDEX idx_properties_id ON properties(id);
-CREATE INDEX idx_properties_name ON properties(name);
+
+-- Index for review's property ID
+CREATE INDEX idx_reviews_property_id ON reviews(property_id);
+
+-- Performance measurement with EXPLAIN ANALYZE
+EXPLAIN ANALYZE SELECT * FROM bookings WHERE user_id = 2;
+
+EXPLAIN ANALYZE
+SELECT users.name, bookings.start_date
+FROM users
+JOIN bookings ON users.id = bookings.user_id;
