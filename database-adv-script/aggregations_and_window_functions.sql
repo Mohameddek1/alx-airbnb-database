fc7becfab578cba2f36d6ceaf69eb9f1ed77bd69
Mohameddek1 <mohadek2004@gmail.com>
2025-07-06 19:24:03 +0300

    alx-adv-script

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,12 +9,24 @@ LEFT JOIN bookings ON users.id = bookings.user_id
 GROUP BY
   users.id, users.name;
 
--- Window Function: Rank properties based on total number of bookings
+-- Window Function: Rank properties based on total number of bookings using RANK()
 SELECT
   p.id AS property_id,
   p.name AS property_name,
   COUNT(b.id) AS booking_count,
   RANK() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
+FROM
+  properties p
+LEFT JOIN bookings b ON p.id = b.property_id
+GROUP BY
+  p.id, p.name;
+
+-- Window Function: Row number for properties based on total number of bookings using ROW_NUMBER()
+SELECT
+  p.id AS property_id,
+  p.name AS property_name,
+  COUNT(b.id) AS booking_count,
+  ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS row_number
 FROM
   properties p
 LEFT JOIN bookings b ON p.id = b.property_id
